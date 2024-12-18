dd35b1685fa7b8d4af2848913cc366fe78213594
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-12-18 12:38:45 +0600

    Update Condition.py

--- Condition.py
+++ Condition.py
@@ -8,3 +8,13 @@ if rd==4:
     end=True;
     print(start);
     print(end);
+if skill==1:
+    print(f"Choosable team is:{team[0]}");
+if skill==2:
+    print(f"Choosable team is:{team[1]}");
+if skill==3:
+    print(f"Choosable team is:{team[2]}");
+if skill==4:
+    print(f"Choosable team is:{team[3]}");
+if skill==5:
+    print(f"Choosable team is:{team[4]}");
