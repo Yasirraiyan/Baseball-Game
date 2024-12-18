510b24a02ee2a1cec492267d8b5431608fd34b1b
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-12-18 12:46:25 +0600

    Create Ninth ennings.py

---
+++ Ninth ennings.py
@@ -0,0 +1,14 @@
+ def ninthennings(ennings,out):
+      if ennings==9and not out:
+         s9=score+50;
+          life=life/512;
+          chance-=9;
+          print(f"The present score is:{s9}.The present life is:{life}.The present life is{chance}")
+          print("9th ennings passed successfully.Go tenth step");
+     if  not out or chance==0 or life==0:
+         print("Fail!")
+                                   
+    allpassed=True;    
+      print(allpassed);
+      print()
+      Finalscore=score1+score2+score3+score4+score5+score6+score7+score8+score9;
